a148dacaf5722d422ce3655b8386f51566dd8edb
Bharath <enthusiasticperson02@gmail.com>
2026-03-15 20:06:28 +0000

    sample SQlite is inserted and tested

--- qt_addressbook/mainwindow.cpp
+++ qt_addressbook/mainwindow.cpp
@@ -1,11 +1,40 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QDebug>
+#include <QTableWidgetItem>
+
+void MainWindow::loadContacts()
+{
+    ui->contactsTable->setRowCount(0);
+
+    QSqlQuery query("SELECT name, mobile, email, birthday FROM contacts");
+
+    int row = 0;
+
+    while(query.next())
+    {
+        ui->contactsTable->insertRow(row);
+
+        ui->contactsTable->setItem(row, 0, new QTableWidgetItem(query.value(0).toString()));
+        ui->contactsTable->setItem(row, 1, new QTableWidgetItem(query.value(1).toString()));
+        ui->contactsTable->setItem(row, 2, new QTableWidgetItem(query.value(2).toString()));
+        ui->contactsTable->setItem(row, 3, new QTableWidgetItem(query.value(3).toString()));
+
+        row++;
+    }
+}
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    loadContacts();
 }
 
 MainWindow::~MainWindow()
